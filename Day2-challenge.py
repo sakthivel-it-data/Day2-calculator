64392c1a95dd8c59284fc11a4191923b50287640
sakthivel-it-data <sakthivel.it.data@gmail.com>
2026-07-04 16:26:56 +0530

    Add calculator code for Day 2

---
+++ Day2-challenge.py
@@ -0,0 +1,10 @@
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+sum_result = a+b
+difference = a-b
+product = a*b
+division = a/b
+print("Sum:", sum_result)
+print("Difference:", difference)
+print("Product:",product)
+print("Division:",division)
